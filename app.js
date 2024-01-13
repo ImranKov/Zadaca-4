77b1eab402e0720575fae9a58cb1103d60fee3c5
ImranKov <imran.kovacevic@yahoo.com>
2024-01-13 17:12:46 +0100

    opocionalni zadatak

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ function ispisiRijec(rijec) {
 }
 
 ispisiRijec("polica");
+
+//Opcionalni zadatak
+function ispisiParneBrojeve(broj) {
+    if (broj === 2) {
+        console.log(broj);
+        return;
+    }
+    if (broj % 2 === 0) {
+        console.log(broj);
+    }
+    ispisiParneBrojeve(broj - 2);
+}
+
+ispisiParneBrojeve(10);
